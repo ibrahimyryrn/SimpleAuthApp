d0acdb43c06d57dcb1d33ceafd008358970ed67e
ibrahimyryrn <ibrahimyeryaran@gmail.com>
2024-06-30 02:34:58 +0300

    update

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,60 +1,65 @@
-import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
 import "./LoginPage.css";
+import { useNavigate } from "react-router-dom";
 
-function LoginPage({ setIsAuth }) {
+function LoginPage({ setIsAuth, setIsAuthenticated }) {
   const [username, setUserName] = useState("");
   const [password, setPassword] = useState("");
-  const [token, setToken] = useState(null);
+  // const [token, setToken] = useState(false);
 
   const navigate = useNavigate();
-
-  useEffect(() => {
-    if (token) {
-      setIsAuth(true);
-      navigate("/home");
-    }
-  }, [token, setIsAuth, navigate]);
 
   async function handleLogin(e) {
     e.preventDefault();
 
-    // LocalStorage'dan kullanıcı adı ve şifreyi al
-    const savedUsername = localStorage.getItem("username");
-    const savedPassword = localStorage.getItem("password");
+    try {
+      const response = await axios.get("http://localhost:5000/users");
 
-    if (username === savedUsername && password === savedPassword) {
-      setToken("fake-jwt-token");
-    } else {
-      alert("Invalid credentials");
+      const user = response.data.find(
+        (u) => u.username === username && u.password === password
+      );
+
+      if (user) {
+        // setToken("fake - jwt - token"); Normalde backend'den alınan gerçek token buraya konur
+        setIsAuthenticated(true);
+        setIsAuth(true);
+        navigate("/home");
+      } else {
+        alert("Invalid credentials");
+      }
+    } catch (error) {
+      console.error("Login error:", error);
     }
   }
 
   return (
-    <div className="login-page">
-      <div className="login-container">
-        <h2 className="login-title">Login Page</h2>
-        <form className="login-form" onSubmit={handleLogin}>
-          <div className="form-group">
-            <label>Username</label>
-            <input
-              type="text"
-              value={username}
-              onChange={(e) => setUserName(e.target.value)}
-            />
-          </div>
-          <div className="form-group">
-            <label>Password</label>
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-          <button className="login-button">Login</button>
-        </form>
+    <>
+      <div className="login-page">
+        <div className="login-container">
+          <h2 className="login-title">Login Page</h2>
+          <form className="login-form" onSubmit={handleLogin}>
+            <div className="form-group">
+              <label>Username</label>
+              <input
+                type="text"
+                value={username}
+                onChange={(e) => setUserName(e.target.value)}
+              />
+            </div>
+            <div className="form-group">
+              <label>Password</label>
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <button className="login-button">Login</button>
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
