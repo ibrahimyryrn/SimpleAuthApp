5d9fff34c02e71c4f52c9652313a219a1c3303b0
ibrahimyryrn <ibrahimyeryaran@gmail.com>
2024-06-30 18:19:13 +0300

    update

--- src/pages/SignInPage.jsx
+++ src/pages/SignInPage.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./SignInPage.css";
+import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 function SignInPage({ setIsAuthenticated, setIsAuth }) {
@@ -8,44 +9,57 @@ function SignInPage({ setIsAuthenticated, setIsAuth }) {
 
   const navigate = useNavigate();
 
-  function handleSignin(e) {
+  async function handleSignin(e) {
     e.preventDefault();
 
-    // Kullanıcı adı ve şifreyi localStorage'a kaydet
-    localStorage.setItem("username", username);
-    localStorage.setItem("password", password);
+    try {
+      const response = await axios.post("http://localhost:5000/users", {
+        username,
+        password,
+      });
 
-    alert("Valid credentials");
-    setIsAuthenticated(true);
-    setIsAuth(true);
-    navigate("/home");
+      // Gerçek kimlik doğrulamada token burada elde edilir
+      if (response.data.password.length > 0) {
+        // setToken("fake-jwt-token");  Normalde backend'den alınan gerçek token buraya konur
+        alert("Valid credentials");
+        setIsAuthenticated(true);
+        setIsAuth(true);
+        navigate("/home");
+      } else {
+        alert("Invalid credentials");
+      }
+    } catch (error) {
+      console.error("Login error:", error);
+    }
   }
 
   return (
-    <div className="signin-page">
-      <div className="signin-container">
-        <h2 className="signin-title">Sign in Page</h2>
-        <form className="signin-form" onSubmit={handleSignin}>
-          <div className="form-group">
-            <label>Username</label>
-            <input
-              type="text"
-              value={username}
-              onChange={(e) => setUserName(e.target.value)}
-            />
-          </div>
-          <div className="form-group">
-            <label>Password</label>
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-          <button className="signin-button">Sign in</button>
-        </form>
+    <>
+      <div className="signin-page">
+        <div className="signin-container">
+          <h2 className="signin-title">Sign in Page</h2>
+          <form className="signin-form" onSubmit={handleSignin}>
+            <div className="form-group">
+              <label>Username</label>
+              <input
+                type="text"
+                value={username}
+                onChange={(e) => setUserName(e.target.value)}
+              />
+            </div>
+            <div className="form-group">
+              <label>Password</label>
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <button className="signin-button">Sign in</button>
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
